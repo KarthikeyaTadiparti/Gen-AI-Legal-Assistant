5b22b7b6d094b8145435949dc20d8d0e4c9806b1
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2025-03-05 15:50:54 +0530

    configured react, tailwind and shadcn

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+
+    return (
+        <div>
+          <h1 className="text-2xl bg-red-500">Hello</h1>
+          <Button>Click me</Button>
+        </div>
+    );
+}
+
+export default App;
